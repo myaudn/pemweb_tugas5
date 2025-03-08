4bb272c02068a12bf0c6e6b5dd66e023dcb45b05
myaudn <mayaaudina040@gmail.com>
2025-03-08 15:28:26 +0700

    design fix

--- result.php
+++ result.php
@@ -16,18 +16,41 @@ if (!isset($_SESSION['name']) || !isset($_SESSION['specialty']) || !isset($_SESS
 <body>
     <div class="container">
         <div class="displaycv">
-                <?php
-                if (isset($_SESSION['pic_path'])) {
-                    var_dump($_SESSION['pic_path']);
-                    echo "<div class='picholder'><img src='" . htmlspecialchars($_SESSION['pic_path']) . "' alt='Foto Profil'></div>";
-                }
-                ?>
-            <p><?php echo $_SESSION['name']; ?></p>
-            <p><?php echo $_SESSION['specialty']; ?></p>
-            <p><strong>Riwayat Pendidikan:</strong> <?php echo nl2br($_SESSION['education']); ?></p>
-            <p><strong>Email:</strong> <?php echo $_SESSION['email']; ?></p>
+            <div class="upper">
+                <div class="mainpart">
+                    <?php
+                        if (isset($_SESSION['pic_path'])) {
+                            echo "<div class='picholder'><img src='" . htmlspecialchars($_SESSION['pic_path']) . "' alt='Foto Profil'></div>";
+                        }
+                    ?>
+                    <p id="name"><?php echo $_SESSION['name']; ?></p>
+                    <p id="specialty"><?php echo $_SESSION['specialty']; ?></p>
+                </div>
+                <div class="co-main">
+                    <p id="about"><?php echo nl2br($_SESSION['about']); ?></p>
+                    <p class="subtitle">Abilities</p><hr style="border: 1px solid  #673C33; width: 90%; margin: 0 20px 0 auto;">
+                    <p id="hardskill"><?php echo nl2br($_SESSION['hardskill']); ?></p>
+                </div>
+            </div>
+            
+            <div class="lower">
+                <div class="expe">
+                <p class="subtitle"><strong>Riwayat Pendidikan</strong><br></p>
+                <p><?php echo nl2br($_SESSION['education']); ?></p>
+                </div>
+                <p id="softskill"><?php echo nl2br($_SESSION['softskill']); ?></p>
+                <div class="contact">
+                    <p class="subtitle">No. HP:</p>
+                    <p><?php echo $_SESSION['num']; ?></p>
+                    <p class="subtitle">Email:</p>
+                    <p><?php echo $_SESSION['email']; ?></p>
+                    <p class="subtitle">Github:</p>
+                    <p><?php echo $_SESSION['gith']; ?></p>
+                </div>
+            </div>
 
-            <p><a href="reset.php">Log out</a></p>
+
+            <p id="logout"><a href="reset.php">Log out</a></p>
         </div>
     </div>
 
