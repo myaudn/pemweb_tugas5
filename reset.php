2f5e141ae0b1fdb74141a85d63575a0ae948668f
myaudn <mayaaudina040@gmail.com>
2025-03-08 16:01:55 +0700

    fixing

--- reset.php
+++ reset.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+
+if (isset($_SESSION['pic_path'])) {
+    if (file_exists($_SESSION['pic_path'])) {
+        unlink($_SESSION['pic_path']);
+    }
+    unset($_SESSION['pic_path']);
+}
+
 session_destroy();
 header("Location: index.php");
 exit();
